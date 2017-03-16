9423442c239f30752fd6adb50dccf156351c990a
Karim Alibhai <karim@alibhai.co>
2017-03-16 02:34:37 -0400

    Add clustering to server

--- index.js
+++ index.js
@@ -5,21 +5,13 @@
  * Copyright (C) 2017 Karim Alibhai.
  */
 
-const path = require('path')
-    , express = require('express')
-    , app = express()
-    , http = require('http').createServer(app)
-    , io = require('socket.io')(http)
+const cluster = require('cluster')
+const CONCURRENCY = process.env.WEB_CONCURRENCY || 1
 
-app.use(require('morgan')('dev'))
-app.use(express.static(path.join(__dirname, 'public')))
-
-io.on('connection', sock => {
-  sock.on('data', data => {
-    console.warn(data)
-  })
-})
-
-http.listen(process.env.PORT || 8080, () =>
-  console.log('Listening :%s', http.address().port)
-)
+if (cluster.isMaster) {
+  for (let i = 0; i < CONCURRENCY; i += 1) {
+    cluster.fork()
+  }
+} else {
+  require('./lib')
+}

74e9fba075be76fc0be58bdee6eaf3be77955a91
Karim Alibhai <karim@alibhai.co>
2017-03-20 21:43:10 -0400

    Disable clustering (because socket.io)

--- index.js
+++ index.js
@@ -6,23 +6,9 @@
  */
 
 require('babel-register')
+require('./lib/env')
 
-const cluster = require('cluster')
-const CONCURRENCY = process.env.WEB_CONCURRENCY || 1
-
-if (cluster.isMaster) {
-  for (let i = 0; i < CONCURRENCY; i += 1) {
-    cluster.fork()
-  }
-
-  cluster.on('exit', (worker, code) => {
-    console.log('Worker %s died with status code %s', worker.process.pid, code)
-  })
-} else {
-  require('./lib/env')
-
-  const http = require('./lib').default
-  http.listen(process.env.PORT || 8080, () =>
-    console.log('Listening :%s', http.address().port)
-  )
-}
+const http = require('./lib').default
+http.listen(process.env.PORT || 8080, () =>
+  console.log('Listening :%s', http.address().port)
+)
